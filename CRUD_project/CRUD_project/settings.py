636244351feb99dd334d2061ae16291e8aa63c84
sairanjith101 <139247459+sairanjith101@users.noreply.github.com>
2024-03-28 15:08:21 +0530

    Update settings.py

--- CRUD_project/CRUD_project/settings.py
+++ CRUD_project/CRUD_project/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'CRUD_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD':'12345',
+        'PORT': '3306',        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
